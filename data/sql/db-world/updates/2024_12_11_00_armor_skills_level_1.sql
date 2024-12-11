a371db8f98c2e2f2a34a640341d687d8fef8a857
Idair F. Guido <idair_guido@hotmail.com>
2024-12-11 13:34:14 -0300

    fix: ids on armor_skills_level_1.sql

--- data/sql/db-world/updates/2024_12_11_00_armor_skills_level_1.sql
+++ data/sql/db-world/updates/2024_12_11_00_armor_skills_level_1.sql
@@ -1,54 +1,55 @@
 SET
-@HumanID		:= 1,
-@OrcID			:= 2,
-@DwarfID		:= 3,
-@NightelfID		:= 4,
-@UndeadID		:= 5,
-@TaurenID		:= 6,
-@GnomeID		:= 7,
-@TrollID		:= 8,
-@BloodElfID		:= 10,
-@DraeneiID		:= 11,
-@WarriorID		:= 1,
-@PaladinID		:= 2,
-@HunterID		:= 3,
-@RogueID		:= 4,
-@PriestID		:= 5,
-@ShamanID		:= 7,
-@MailSpellID	:= 413,
-@PlateSpellID	:= 293,
-@Mailnote	:= "Mail",
-@Platenote	:= "Plate Mail";
+@HumanMask			:= 1,
+@OrcMask			:= 2,
+@DwarfMask			:= 4,
+@NightelfMask		:= 8,
+@UndeadMask			:= 16,
+@TaurenMask			:= 32,
+@GnomeMask			:= 64,
+@TrollMask			:= 128,
+@BloodElfMask		:= 512,
+@DraeneiMask		:= 1024,
+@WarriorMask		:= 1,
+@PaladinMask		:= 2,
+@HunterMask			:= 4,
+@RogueMask			:= 8,
+@PriestMask			:= 16,
+@DeathKnightMask	:= 32,
+@ShamanMask			:= 64,
+@MailSkillID		:= 413,
+@PlateSkillID		:= 293,
+@Mailnote			:= "Mail",
+@Platenote			:= "Plate Mail";
 
 ## [1] Warrior Plate Mail
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @HumanID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @NightelfID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @UndeadID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @GnomeID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollID, @WarriorID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiID, @WarriorID, @PlateSpellID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @HumanMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @NightelfMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @UndeadMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @GnomeMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiMask, @WarriorMask, @PlateSkillID, 0, @Platenote);
 
 ## [2] Paladin Plate Mail
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @HumanID, @PaladinID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfID, @PaladinID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @BloodElfID, @PaladinID, @PlateSpellID, 0, @Platenote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiID, @PaladinID, @PlateSpellID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @HumanMask, @PaladinMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfMask, @PaladinMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @BloodElfMask, @PaladinMask, @PlateSkillID, 0, @Platenote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiMask, @PaladinMask, @PlateSkillID, 0, @Platenote);
 
 ## [3] Hunter Mail
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcID, @HunterID, @MailSpellID, 0, @Mailnote); 
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfID, @HunterID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @NightelfID, @HunterID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenID, @HunterID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollID, @HunterID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @BloodElfID, @HunterID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiID, @HunterID, @MailSpellID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcMask, @HunterMask, @MailSkillID, 0, @Mailnote); 
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DwarfMask, @HunterMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @NightelfMask, @HunterMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenMask, @HunterMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollMask, @HunterMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @BloodElfMask, @HunterMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiMask, @HunterMask, @MailSkillID, 0, @Mailnote);
 
 ## [7] Shaman Mail
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcID, @ShamanID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenID, @ShamanID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollID, @ShamanID, @MailSpellID, 0, @Mailnote);
-INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiID, @ShamanID, @MailSpellID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @OrcMask, @ShamanMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TaurenMask, @ShamanMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @TrollMask, @ShamanMask, @MailSkillID, 0, @Mailnote);
+INSERT INTO playercreateinfo_skills (playercreateinfo_skills.racemask, playercreateinfo_skills.classmask, playercreateinfo_skills.skill, playercreateinfo_skills.rank, playercreateinfo_skills.Comment) VALUES ( @DraeneiMask, @ShamanMask, @MailSkillID, 0, @Mailnote);
 
